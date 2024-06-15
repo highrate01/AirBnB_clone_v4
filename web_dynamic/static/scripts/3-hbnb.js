8bb26d308f2a516ed00ed16257cc3cf5504f9566
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-15 14:27:58 +0000

    updated

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,60 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+    const amenities = {};
+    $('input[type="checkbox"]').click(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+        if ($(this).prop('checked') === true) {
+            amenities[amenityId] = amenityName;
+        } else if ($(this).prop('checked') === false) {
+            delete amenities[amenityId];
+        }
+        const amenityList = Object.values(amenities).join(', ');
+        if (amenityList.length > 30) {
+            $('.amenities h4').text(amenityList.slice(0, 29) + '...');
+        } else {
+            $('.amenities h4').text(amenityList);
+        }
+        if (Object.keys(amenities).length === 0) {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+
+//load place
+ $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      data.forEach(function (place) {
+        const article = `
+          <article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">${place.description}</div>
+          </article>
+        `;
+        $('.places').append(article);
+      });
+    }
+  });
+});
