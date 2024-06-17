cd9dc6eb95731ee5035b2be2087fc720696fd55d
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-17 22:21:41 +0000

    updated

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,18 +4,18 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid;
+import uuid
 
 # flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 # begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    close session after each request
     """
     storage.close()
 
@@ -31,14 +31,16 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
+    cache_id = uuid.uuid4()
+
     return render_template('0-hbnb.html',
-                           cache_id=uuid.uuid4(),
+                           cache_id=cache_id,
                            states=states,
                            amens=amens,
                            places=places,
                            users=users)
 
+
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
+    """MAIN Flask App"""
     app.run(host='0.0.0.0', port=5000)
--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid;
+import uuid
 
 # flask setup
 app = Flask(__name__)
@@ -15,8 +15,7 @@ app.url_map.strict_slashes = False
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    close session after each request
     """
     storage.close()
 
@@ -26,18 +25,21 @@ def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
     """
+    cache_id = uuid.uuid4()
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
+
     return render_template('4-hbnb.html',
-                           cache_id=uuid.uuid4(),
+                           cache_id=cache_id,
                            states=states,
                            amens=amens,
                            places=places,
                            users=users)
+
 
 if __name__ == "__main__":
     """
